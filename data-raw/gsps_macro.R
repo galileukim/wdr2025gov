2e6f3c59b832e94e108886fe145c2e11d47003f3
Galileu Kim <galileukim@worldbank.org>
2025-04-03 19:08:07 -0400

    clean gsps data and use it
    generate novel datasets using the gsps data, harmonizing questions.

---
+++ data-raw/gsps_macro.R
@@ -0,0 +1,63 @@
+## code to prepare `gsps_macro` dataset goes here
+library(readxl)
+library(dplyr)
+library(janitor)
+library(labelled)
+library(stringr)
+library(here)
+
+# circumvent firewall for download
+options(download.file.method ="curl", download.file.extra ="-k -L")
+
+lcl <-  here("data-raw", "input", "gsps_macro.xlsx")
+
+if(!file.exists(lcl)){
+  download.file(
+    "https://www.globalsurveyofpublicservants.org/sites/default/files/2023-11/GSPS_Indicators_Dataset_11_10_23.xlsx",
+    destfile = here("data-raw", "input", "gsps_macro.xlsx"),
+    overwrite = TRUE
+  )
+}
+
+gsps_input <- read_xlsx(
+  here("data-raw", "input", "gsps_macro.xlsx"),
+  sheet = 1
+  ) |>
+  clean_names()
+
+gsps_standards <- gsps_input |>
+  mutate(
+    topic_group = case_when(
+      str_detect(topic, "(?i)Recruitment") ~ "Recruitment",
+      str_detect(topic, "(?i)Performance") ~ "Performance"
+    ),
+    indicator_group = case_when(
+      str_detect(question_text, "(?i)written exam") & !str_detect(question_text, "skewed") ~ "Recruitment standard: written examination",
+      indicator %in% c("Politicization", "Criteria (political connections)") & str_detect(question_text, "first job|hire|hiring") ~ "Recruitment standard: political connections",
+      # indicator == "Criteria (professional experience)" ~ "Recruitment standard: professional experience",
+      # indicator == "Criteria (education)" ~ "Recruitment standard: education",
+      indicator == "Assessment (none)" ~ "Recruitment standard: none",
+      indicator %in% c("Performance evaluation (performance evaluated)", "Performance evaluation (last 2 years)", "Performance evaluated") ~ "Performance standard: evaluation",
+      indicator == "Performance evaluation (promotion)" | (topic_group == "Performance" & str_detect(question_text, "promotion") & !str_detect(question_text, "transparent and fair")) ~ "Performance standard: promotion",
+      T ~ "Other"
+    )
+  )
+
+gsps_national <- gsps_standards |>
+  filter(
+    group == "All"
+  )
+
+gsps_region <- gsps_standards |>
+  filter(
+    group == "Region"
+  )
+
+gsps_institution <- gsps_standards |>
+  filter(
+    group == "Institution"
+  )
+
+usethis::use_data(gsps_national, overwrite = TRUE)
+usethis::use_data(gsps_region, overwrite = TRUE)
+usethis::use_data(gsps_institution, overwrite = TRUE)
