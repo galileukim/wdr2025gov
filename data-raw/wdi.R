2015d0b26e382a94e6c5b3d2a56b69d232864e45
Galileu Kim <galileukim@worldbank.org>
2025-04-08 17:27:29 -0400

    produce gdp per capita data
    using prosperity data 360 api

--- data-raw/wdi.R
+++ data-raw/wdi.R
@@ -1,14 +1,31 @@
 ## code to prepare `wdi` dataset goes here
-wdi <- fetch_prosperitydata360_data(
+wdi_gdp_pc_input <- fetch_prosperitydata360_data(
   "WB.WDI",
   "WB.WDI.NY.GDP.PCAP.PP.KD"
-)
+) |>
+  as_tibble()
 
-eout <- req_perform(req)
+wdi_gdp_pc <- wdi_gdp_pc_input |>
+  select(
+    country_code = COUNTRY_CODE,
+    indicator_id = INDICATOR_ID,
+    year = CAL_YEAR,
+    value = IND_VALUE
+  ) |>
+  filter(
+    country_code != "AGGREGATE"
+  ) |>
+  pivot_wider(
+    names_from = indicator_id,
+    values_from = value
+  ) |>
+  rename(
+    gdp_per_capita_ppp_2017 = `WB.WDI.NY.GDP.PCAP.PP.KD`
+  ) |>
+  mutate(
+    year = as.numeric(
+      str_sub(year, -4, -1)
+    )
+  )
 
-query_json <- resp_body_json(resp = eout)
-
-tibble(json = resp_body_json(resp = eout)[["value"]]) |>
-  unnest_wider(col = json)
-
-usethis::use_data(wdi, overwrite = TRUE)
+usethis::use_data(wdi_gdp_pc, overwrite = TRUE)
