f79a215849cc7d23e93414272297b2ad18a2a2c6
Galileu Kim <galileukim@worldbank.org>
2025-04-11 11:21:03 -0400

    improve documentation and strengthen wwbi

--- data-raw/source/05-wwbi.R
+++ data-raw/source/05-wwbi.R
@@ -1,12 +1,15 @@
 ## code to prepare `wwbi` dataset goes here
-wwbi <- fetch_prosperitydata360_data(
+library(dplyr)
+library(tidyr)
+library(stringr)
+
+wwbi_input <- fetch_prosperitydata360_data(
   "WB.WWBI",
   "WB.WWBI.BI.PWK.PUBS.NO"
 ) |>
   as_tibble()
 
-
-wdi_gdp_pc <- wdi_gdp_pc_input |>
+wwbi <- wwbi_input |>
   select(
     country_code = COUNTRY_CODE,
     indicator_id = INDICATOR_ID,
@@ -21,12 +24,18 @@ wdi_gdp_pc <- wdi_gdp_pc_input |>
     values_from = value
   ) |>
   rename(
-    gdp_per_capita_ppp_2017 = `WB.WDI.NY.GDP.PCAP.PP.KD`
+    public_paid_employee = `WB.WWBI.BI.PWK.PUBS.NO`
   ) |>
   mutate(
     year = as.numeric(
       str_sub(year, -4, -1)
     )
+  )
+
+wwbi <- wwbi |>
+  left_join(
+    countryclass,
+    by = c("country_code")
   )
 
 usethis::use_data(wwbi, overwrite = TRUE)
